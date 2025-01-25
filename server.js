0bff30eb8ca04a7a780129b9db8f0cbafd1d79c9
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-01-24 23:39:39 -0300

    update api

--- server.js
+++ server.js
@@ -1,18 +1,61 @@
 import express, { request } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-const users = [];
-
-app.post("/users", (req, res) => {
-  users.push(req.body);
+app.post("/users/:id", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      name: req.body.name,
+      age: req.body.age,
+      email: req.body.email,
+    },
+  });
 
   res.status(201).json(req.body);
 });
 
-app.get("/users", (req, res) => {
+app.put("/users", async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      name: req.body.name,
+      age: req.body.age,
+      email: req.body.email,
+    },
+  });
+
+  res.status(201).json(req.body);
+});
+
+app.get("/users", async (req, res) => {
+  if (req.query) {
+    let users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.email,
+        age: req.body.age,
+      },
+    });
+  } else {
+    const users = await prisma.user.findMany();
+  }
   res.status(200).json(users);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.status(200).json({ message: "Deletado com sucesso!" });
 });
 
 app.listen(3000);
@@ -24,4 +67,6 @@ app.listen(3000);
  -listar todos os users
  -editar os users
  -deleter os users
+<kobamamikael></kobamamikael>
+ O8jswFP98CHwAurm
 */
